23095b1925a9a21aeb71d120c1be96a5b4175391
Rakesh Lahkar <rakesh@Rakeshs-MacBook-Air.local>
2020-06-09 12:42:51 +0530

    Updates

---
+++ precache-manifest.1472280159b6197c1a173d42d2e59287.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cccab90a69edbdfeba1756b1c39a90ee",
+    "url": "/rakz25/counter-app.git/index.html"
+  },
+  {
+    "revision": "aaf5ee076edd5a5f706b",
+    "url": "/rakz25/counter-app.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "109f27b4aa0b3e4082f8",
+    "url": "/rakz25/counter-app.git/static/js/2.391a5d1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rakz25/counter-app.git/static/js/2.391a5d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf5ee076edd5a5f706b",
+    "url": "/rakz25/counter-app.git/static/js/main.3daba0fe.chunk.js"
+  },
+  {
+    "revision": "6fc9dea2ad675efb3276",
+    "url": "/rakz25/counter-app.git/static/js/runtime-main.900e6b3f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rakz25/counter-app.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
