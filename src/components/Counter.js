c5f3026333c870d912d12e257d611311ac67573a
Rakesh Lahkar <rakeshlorentz@gmail.com>
2020-06-09 17:22:07 +0530

    message

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,24 +2,38 @@ import React ,{ Component } from "react"
 
 class Counter extends Component{
      constructor(props){
-            super(props)
+            super(props);
             this.state = {count:0}
-            this.handleClick = this.handleClick.bind(this)
+           // this.handleClick = this.handleClick.bind(this)
      }
-     handleClick(){
-           this.setState({count: this.state.count+1})
+    
+    //  handleClick = => {
+    //        this.setState({count: this.state.count+1})
 
-     }
- 
+    //  }
+
+    increment = () => {
+        this.setState({count:this.state.count+1});
+    }
+    decrement = () => {
+        this.setState({count:this.state.count-1});
+    }
+    
+    
      render(){
               return(
-                  <button onClick = {this.handleClick}>Counter{this.state.count}</button>
-              )
+                  <div classname="Counter">
+                  <button onClick={this.increment} classname="counter">+</button>
+                  <button onClick={this.decrement} classname="counter">-</button>
+
+                  <h2>{this.state.count}</h2>
+                  </div>
+                  );
 
      }
+    }
 
 
-}
 export default Counter
 
 
